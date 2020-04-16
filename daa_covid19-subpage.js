aa380a18280f86d5c5488b513eb67c83201e32a3
Virtual Inc <47217495+webadminvirtual@users.noreply.github.com>
2020-04-16 10:25:33 -0400

    Update daa_covid19-subpage.js

--- daa_covid19-subpage.js
+++ daa_covid19-subpage.js
@@ -2,7 +2,7 @@
 
 document.querySelector('.subpage-content').innerHTML += pageContent
             var sheetIdentifier = (document.querySelector('.subpage-content')) ? document.querySelector('.subpage-content').firstElementChild.innerText.replace('COVID Resource Topics - ', '') : 'Digital Data, Trends & Research'
-            gsheets.getWorksheet('1mrle0mhDNHT98JIcf_cyxuke45lgrCUMl8O4gp__qIQ', sheetIdentifier)
+            gsheets.getWorksheet('1mrle0mhDNHT98JIcf_cyxuke45lgrCUMl8O4gp__qIQ', sheetIdentifier.toLowerCase())
               .then(res => {
                     $('#covid').DataTable({
                   data: res.data,
